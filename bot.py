10ee280bcbaad76ef1dc2c89266aa4ec2b757c07
moaidalsihli-source <moaidalsihli@icloud.com>
2026-02-23 04:08:16 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,11 @@
 import os
 import requests
-import time
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": text}
-    requests.post(url, data=data)
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = {"chat_id": CHAT_ID, "text": "اختبار مباشر 🔥"}
 
-# تجربة إرسال من 1 إلى 100
-for i in range(1, 101):
-    send_message(str(i))
-    time.sleep(2)  # يرسل كل رقم كل ثانيتين
+r = requests.post(url, data=data)
+print(r.text)
