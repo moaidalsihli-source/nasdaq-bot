42f5b0ce88bf9bcb43e4b8fdcd4a1745ca8e2393
moaidalsihli-source <moaidalsihli@icloud.com>
2026-02-23 05:00:26 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import os
 import requests
+import time
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": "🔥 البوت شغال 100% 🔥"
-}
+for i in range(1, 51):
+    data = {
+        "chat_id": CHAT_ID,
+        "text": str(i)
+    }
 
-r = requests.post(url, data=data)
-print(r.text)
+    r = requests.post(url, data=data)
+    print(f"Sent {i}:", r.text)
+
+    time.sleep(1)  # انتظار ثانية بين كل رسالة
